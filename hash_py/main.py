c2a6a404469667b236863a959d5f938e37fbf4bc
sp-ino <spinovale97@gmail.com>
2022-05-08 23:13:58 +0200

    Start creating python implementation

---
+++ hash_py/main.py
@@ -0,0 +1,14 @@
+"""
+A python implementation of SHA-256
+
+Copyright (c) 2022 Valerio Spinogatti
+Licensed under GNU license
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
